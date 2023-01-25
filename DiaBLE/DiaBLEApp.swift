ecc8caf1b0b7e6e47ce6f2b6d11006cafa656ce1
gui-dos <7220550+gui-dos@users.noreply.github.com>
2023-01-25 22:10:31 +0100

    Xcode 14 template

---
+++ DiaBLE/DiaBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaBLEApp.swift
+//  DiaBLE
+//
+//  Created by Guido Soranzio on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DiaBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
