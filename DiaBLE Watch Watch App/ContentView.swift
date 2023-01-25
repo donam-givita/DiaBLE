a03c7c2db33cfa86f087aec55eb3e7bfccf8e08b
gui-dos <7220550+gui-dos@users.noreply.github.com>
2023-01-25 23:24:51 +0100

    Add Watch App

---
+++ DiaBLE Watch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiaBLE Watch Watch App
+//
+//  Created by Guido Soranzio on 25/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiaBLE Watch Watch App/DiaBLE_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaBLE_WatchApp.swift
+//  DiaBLE Watch Watch App
+//
+//  Created by Guido Soranzio on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DiaBLE_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
