623cbd9cb03f635976e918a3fd3b3e48730efefa
gui-dos <7220550+gui-dos@users.noreply.github.com>
2023-06-13 00:06:15 +0200

    LLU: add `ca` and `eu2` regions

--- DiaBLE Playground.swiftpm/LibreLink.swift
+++ DiaBLE Playground.swiftpm/LibreLink.swift
@@ -83,7 +83,7 @@ class LibreLinkUp: Logging {
     let connectionsEndpoint = "llu/connections"
     let measurementsEndpoint = "lsl/api/measurements"
 
-    let regions = ["us", "eu", "de", "fr", "jp", "ap", "au", "ae"]
+    let regions = ["ae", "ap", "au", "ca", "de", "eu", "eu2", "fr", "jp", "us"]
 
     var regionalSiteURL: String { "https://api-\(settings.libreLinkUpRegion).libreview.io" }
 
